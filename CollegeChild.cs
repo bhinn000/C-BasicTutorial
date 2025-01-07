2722ce5b136ae9b5e94fa8700088f70fca7b725a
Bhintuna <bhintunash@gmail.com>
2025-01-07 15:58:07 +0545

    Add project files.

---
+++ CollegeChild.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace C_Basic
+{
+    public class CollegeChild:UniversityParent //inheriting
+    {
+
+        //CollegeChild() //child's constructor calls "parent's constructor lets say:  abc" implicitly hence "abc" should be public ; while calling parameterized constructor "UniversityParent(string name)" ,cause a problem hence use keyword "base"  here to call "constructor of parent class-which is also parameterised"
+        //{
+        //    Console.WriteLine("Constructor of child class");
+        //}
+
+        //CollegeChild() : base("St.Xavier") //hard coded
+        //{
+        //    Console.WriteLine("Parameterised child class constructor");
+        //}
+
+        CollegeChild(string name) : base(name) //dynamic
+        {
+            Console.WriteLine("Parameterised child class constructor");
+        }
+
+        void RIC()
+        {
+            Console.WriteLine("We are going for a trip to Pokhara");
+        }
+
+        void SportsWeek()
+        {
+            Console.WriteLine("This is SportsWeek");
+        }
+
+        
+          public static void Main()
+        {
+            CollegeChild cc = new CollegeChild("St.Xavier's");
+            //cc.EmployeeInfo();// this method is private hence can't accessed with child's object
+            cc.VacationSchedule();
+
+
+        }
+        //cc.EmployeeInfo(); 
+        //cc.VacationSchedule();
+       
+    }
+}
---
+++ UniversityParent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Basic
+{
+    public class UniversityParent
+    {
+
+        //public UniversityParent()   //constructor , -implicit- , -parameterless-
+        //{
+        //    Console.WriteLine("Constructor of parent class");
+        //}
+
+        public UniversityParent(string name)
+        { //parameterised constructor
+            Console.WriteLine($"My name is {name}");
+        }
+        public void VacationSchedule()
+        {
+            Console.WriteLine("We have winter and summer vacation");
+        }
+
+        public void ExamSchedule() //by default access modifier is "Private"
+        {
+            Console.WriteLine("Board Exam is coming near");
+        }
+
+        private void EmployeeInfo()
+        {
+            Console.WriteLine("We have 350 employees");
+        }
+
+        //static void Main()
+        //{
+        //    UniversityParent up=new UniversityParent();
+        //    up.ExamSchedule();
+        //}
+
+
+
+    }
+}
