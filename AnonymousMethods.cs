d5a8fb6df6cc104cb01b1397eeca66b75d41a5e4
Bhintuna <bhintunash@gmail.com>
2025-01-09 15:20:35 +0545

    Anonymous Method 1

---
+++ AnonymousMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Basic
+{
+    public delegate string GreetingsDelegate(string name1);
+    public class AnonymousMethods
+    {
+        
+
+        static void Main()
+        {
+            //GreetingsDelegate greetingsDelegate = new GreetingsDelegate(Greetings);
+            //String greet= greetingsDelegate("Shyam");
+            //Console.WriteLine(greet);
+
+            //instead of above we can do following: anonymous method, a method with a name
+            GreetingsDelegate greetingsDelegate = delegate (string name) // the return type of this has been defined in the delegate "GreetingsDelegate" as string
+                                                                         // , suitable for few lines of code
+            {
+                return "Hello " + name;
+            };
+        }
+    }
+}
